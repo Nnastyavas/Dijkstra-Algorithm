12d2788eed019674666931afffc358e0c6fff93f
Nnastyavas <45784606+Nnastyavas@users.noreply.github.com>
2020-05-28 14:54:12 +0300

    Add files via upload

---
+++ Алгоритм Дейкстры/Source.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include"DAlgorithm.h"
+#include <fstream>
+#include"input_data_function.h"
+using namespace std;
+
+
+int main() {
+	try {
+		setlocale(LC_ALL, "RUS");
+		ifstream vvod("input.txt");
+		int* path = NULL;
+		int* d = NULL;
+		List<string>* list_fly = new List<string>();
+		string city_Start;
+		string city_End;
+		InputInFile(list_fly, vvod);
+		cout << "Flights: " << endl;
+		for (int i = 0; i < list_fly->get_size(); i++)
+			cout << list_fly->at(i) << endl;
+		cout << "Enter the departure city" << endl;
+		getline(cin, city_Start);
+		cout << "Enter your arrival city" << endl;
+		getline(cin, city_End);
+		AdjList* adj = new AdjList(list_fly);
+		cout << adj->DAlgorithm(city_Start, city_End);
+	}
+	catch (exception & ex) {
+		cout << ex.what() << endl;
+	}
+	system("pause");
+}
